385e71b855048110bce61e36a2f755610d7cee36
a.s.mihaylov <a.s.mihaylov@yandex.ru>
2019-05-15 13:39:24 +0400

    4. Conflict 1.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 int main(int argc, char *argv[]) {  // 1
-	QFile file("error.txt"); // 1
-	QTextStream stream(&file); // 1
+	QFile file("egor.txt"); // 4
+	QTextStream stream(&file), stream2(&file2); // 4
 	QStringList paths = QCoreApplication::libraryPaths(); // 1
 	if (file.open(QIODevice::WriteOnly)) { // 1
 		stream << "LIBS: " << paths.size() << endl; // 1
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {  // 1
 	stream << "NEW LIBS:" << paths.size() << endl; // 2
 	foreach(QString libPath, QCoreApplication::libraryPaths()) { // 2
 		stream << libPath << endl; // 2
-	} // 2
+	} // 
 	stream << "OLD DRIVERS:" << endl; // 3
 	foreach(auto driver, QSqlDatabase::drivers()) { // 3
 		stream << driver << endl; // 3
